504465dffaec9d10f819488f593eb3fdf6b91c6e
YA2JA <41586062+YA2JA@users.noreply.github.com>
2019-04-12 18:37:49 +0200

    -
    -

--- main.py
+++ main.py
@@ -1,38 +1,52 @@
-def BlackPawn(place, x , y):
-	x-=1
-	y-=1
-	place[y].pop(x)
-	place[y].insert(x,"♟")
-	return place
+class Moves():
+	def Pawn(table, x, y):
+		pass
 
-def WhitePawn(place, x , y):
-	x-=1
-	y-=1
-	place[y].pop(x)
-	place[y].insert(x,"♙")
-	return place
+class Black():
 
+				
+	def Pawn(place, x , y):
+		x-=1
+		y-=1
+		place[y].pop(x)
+		place[y].insert(x,"♟")
+		return place
 
-def table():
-	x = 8
-	old_table= [["."]*x for y in range(8)]
-	end_table = AddPions(old_table)
+class White():
+	def Pawn(place, x , y):
+		x-=1
+		y-=1
+		place[y].pop(x)
+		place[y].insert(x,"♙")
+		return place
+
+class board():
+	"""docstring for board"""
+	def table():
+		x = 8
+		old_table= [[""]*x for y in range(8)]
+		end_table = board.AddPions(old_table)
 	
-	for a in end_table:
-		print(a,'\n')
+		for a in end_table:
+			for b in a:
+				print(b, end = "  ")
+			print("\n")
 
-def AddPions(place):
-	for N in range(9):
-		resutlat = BlackPawn(place,N,2)
-	for N in range(9):
-		resutlat = WhitePawn(resutlat, N , 7)
+	def AddPions(place):
+		for N in range(9):
+			resutlat = Black.Pawn(place,N,2)
+		for N in range(9):
+			resutlat = White.Pawn(resutlat, N , 7)
+		return resutlat
+	def update_table(before):
+		for a in (before):
+			for b in a:
+				print(b, end = "  ")
+			print("\n")
 
-
-
-	return resutlat
 
 def main():
-	table()
+	board.table()
 
 
 
